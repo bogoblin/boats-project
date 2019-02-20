cc8fbfe10ff0923f5f41ba0391544fefc8e8fac5
Robert McCann <bogoblin@gmail.com>
2019-02-20 19:54:46 +0000

    Add control style tooltips

--- Assets/UI/ControlStyleDropdown.cs
+++ Assets/UI/ControlStyleDropdown.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class ControlStyleDropdown : MonoBehaviour {
+	// This behavior ensures that the control style dropdown menu always displays the correct tooltip
 
-	// Use this for initialization
+	public GameObject[] Tooltips;
+
 	void Start () {
+		// Initialise the dropdown value to be the saved control style
 		this.GetComponent<Dropdown>().value = PlayerPrefs.GetInt("Control Style");
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		
+		for (int i=0; i<Tooltips.Length; i++) {
+			// Show the tooltip if it is the correct one to show, otherwise hide it
+			bool show = (this.GetComponent<Dropdown>().value == i);
+			Tooltips[i].SetActive(show);
+		}
 	}
 }
