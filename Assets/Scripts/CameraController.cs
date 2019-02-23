eda55ef7b429be2b04520a7cff954321d8abc1d7
Robert McCann <bogoblin@gmail.com>
2019-02-23 15:08:51 +0000

    Stop camera from bobbing up and down

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -11,25 +11,34 @@ public class CameraController : MonoBehaviour {
 		Behind
 	};
 	public CameraMode cameraMode = CameraMode.Checkpoint;
+	private float initialYPosition;
 
-	// Use this for initialization
 	void Start () {
-		
+		initialYPosition = transform.position.y;
 	}
 	
-	// Update is called once per frame
 	void Update () {
+		// Keep camera y position the same
+		Vector3 correctPosition = new Vector3(
+			transform.position.x,
+			initialYPosition,
+			transform.position.z
+		);
+		transform.SetPositionAndRotation(
+			correctPosition,
+			transform.rotation
+		);
+
 		switch (cameraMode) {
 			case CameraMode.Checkpoint:
-			float angleBetween = Vector3.SignedAngle(this.transform.forward, cameraTarget.transform.forward, Vector3.down);
-			//this.transform.eulerAngles = Vector3.Angle Lerp(this.transform.eulerAngles, cameraTarget.transform.eulerAngles, Time.deltaTime*speed);
-			this.transform.Rotate(0, -angleBetween * Time.deltaTime * speed, 0, Space.World);
-			this.transform.eulerAngles = new Vector3(
-				0, this.transform.eulerAngles.y, 0
+			float angleBetween = Vector3.SignedAngle(transform.forward, cameraTarget.transform.forward, Vector3.down);
+			transform.Rotate(0, -angleBetween * Time.deltaTime * speed, 0, Space.World);
+			transform.eulerAngles = new Vector3(
+				0, transform.eulerAngles.y, 0
 			);
 			break;
 			case CameraMode.Behind:
-			this.transform.localEulerAngles = Vector3.zero;
+			transform.localEulerAngles = Vector3.zero;
 			break;
 			default:
 			break;
