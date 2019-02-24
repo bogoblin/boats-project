086ccd1b0fefc4d9f95dcfdcb19e14c5c37388b2
Robert McCann <bogoblin@gmail.com>
2019-02-24 14:54:28 +0000

    Create skeleton AI boat controller

---
+++ Assets/Scripts/Controllers/AIBoatController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIBoatController : MonoBehaviour, IBoatController {
+	private BoatBehavior boatBehavior;
+	public GameObject target;
+	private Weather weather;
+	void Start () {
+		weather = Weather.Instance;
+	}
+	void Update () {
+		
+	}
+	public float GetRudder () {
+		return 1;
+	}
+	public float GetSailPull () {
+		return 0;
+	}
+	public float GetSailTurn () {
+		return 0;
+	}
+}
